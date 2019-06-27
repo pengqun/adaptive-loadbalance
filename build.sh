e45712631e3b7d742090e8beb0008d4324a967e4
楚衡 <pengqun.pq@alibaba-inc.com>
2019-06-27 10:44:47 +0800

    Update build script

--- build.sh
+++ build.sh
@@ -1,20 +1,27 @@
 #!/bin/sh
 
-if [[ "$1" = "consumer" ]]; then
-  echo "build $1"
+if [[ "$1" = "consumer" || "$1" = "all" ]]; then
+  echo "=== build consumer begin ==="
   cd debian-jdk8-adaptive-loadbalance/debian-jdk8-consumer
   rm -rf adaptive-loadbalance
   cp -R /Users/ch/code/race/tianchi2019/adaptive-loadbalance .
   docker build --build-arg user_code_address="https://code.aliyun.com/pengqun/adaptive-loadbalance.git" -t consumer .
   rm -rf adaptive-loadbalance
-elif [[ "$1" = "provider" ]]; then
-  echo "build $1"
+  cd ../../
+  echo "=== build consumer end ==="
+fi
+
+if [[ "$1" = "provider" || "$1" = "all" ]]; then
+  echo "=== build provider begin ==="
   cd debian-jdk8-adaptive-loadbalance/debian-jdk8-provider
   rm -rf adaptive-loadbalance
   cp -R /Users/ch/code/race/tianchi2019/adaptive-loadbalance .
   docker build --build-arg user_code_address="https://code.aliyun.com/pengqun/adaptive-loadbalance.git" -t provider .
   rm -rf adaptive-loadbalance
-else
-  echo "wrong target: $1"
-  exit -1
+  cd ../../
+  echo "=== build provider end ==="
+fi
+
+if [[ "$2" = "up" ]]; then
+  docker-compose up
 fi
--- workspace-gateway/src/main/java/com/aliware/tianchi/loadbalance/AbstractLoadBalance.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/loadbalance/AbstractLoadBalance.java
@@ -1,11 +1,14 @@
 package com.aliware.tianchi.loadbalance;
 
 import com.aliware.tianchi.CommonUtils;
+import com.aliware.tianchi.LogUtils;
 import com.aliware.tianchi.ProviderStats;
 import org.apache.dubbo.common.URL;
 import org.apache.dubbo.rpc.Invocation;
 import org.apache.dubbo.rpc.Invoker;
 import org.apache.dubbo.rpc.cluster.LoadBalance;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
@@ -13,6 +16,12 @@ import java.util.List;
  * @author ./ignore 2019-06-26
  */
 public abstract class AbstractLoadBalance implements LoadBalance {
+
+    private static final Logger logger = LoggerFactory.getLogger(LeastActiveLoadBalance.class);
+
+    static {
+        LogUtils.turnOnDebugLog(logger);
+    }
 
     private static final int DEFAULT_WEIGHT = 1;
 
@@ -36,12 +45,17 @@ public abstract class AbstractLoadBalance implements LoadBalance {
             return 0;
         }
         int successCounter = providerStats.getSuccessCounter();
+        int totalElapsed = providerStats.getTotalElapsed();
         int active = providerStats.getActive() + 1;
         int max = providerStats.getMaxPoolSize();
+        int weight = DEFAULT_WEIGHT;
         if (successCounter > 0) {
-            int totalElapsed = providerStats.getTotalElapsed();
-            return (int) ((10000L * successCounter * max) / (totalElapsed * active));
+            weight = (int) ((10000L * successCounter * max) / (totalElapsed * active));
         }
-        return DEFAULT_WEIGHT;
+        if (logger.isDebugEnabled()) {
+            logger.debug("Weight for {}: {}, success - {}, elapsed - {}, active - {}, max - {}",
+                    providerKey, weight, successCounter, totalElapsed, active, max);
+        }
+        return weight;
     }
 }
