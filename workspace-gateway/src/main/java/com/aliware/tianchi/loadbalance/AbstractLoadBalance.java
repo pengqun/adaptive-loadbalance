bbfa26192bb22880be228b253676ef0a6b527e66
楚衡 <pengqun.pq@alibaba-inc.com>
2019-07-09 09:49:24 +0800

    adjust protect ratio

--- workspace-gateway/src/main/java/com/aliware/tianchi/loadbalance/AbstractLoadBalance.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/loadbalance/AbstractLoadBalance.java
@@ -137,7 +137,7 @@ public abstract class AbstractLoadBalance implements LoadBalance {
     private int getWeightByCapacity(String providerKey, ProviderStats providerStats) {
         int active = providerStats.getActive();
         int max = providerStats.getMaxPoolSize();
-        if (active > max * 0.5) {
+        if (active > max * 0.7) {
             return 0;
         }
 
