d6e73ee8810233cde618d39a4350a9c9d8203c02
楚衡 <pengqun.pq@alibaba-inc.com>
2019-07-12 16:41:06 +0800

    update

--- workspace-gateway/src/main/java/com/aliware/tianchi/loadbalance/AbstractLoadBalance.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/loadbalance/AbstractLoadBalance.java
@@ -143,7 +143,7 @@ public abstract class AbstractLoadBalance implements LoadBalance {
     private int getWeightByCapacity(String providerKey, ProviderStats providerStats) {
         int active = providerStats.getActive();
         int max = providerStats.getMaxPoolSize();
-        if (active > max * 0.8) {
+        if (active > max * 0.5) {
             return 0;
         }
 
